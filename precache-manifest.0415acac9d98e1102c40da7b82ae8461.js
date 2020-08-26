1e355bd91d3c3d25ba85c7edd5394500ee54dd67
bharathjinka09 <bharathjinka09@gmail.com>
2020-08-26 13:36:28 +0530

    Updates

---
+++ precache-manifest.0415acac9d98e1102c40da7b82ae8461.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f987604a61584ca3b3ed862cce215f2",
+    "url": "/quiz-app/index.html"
+  },
+  {
+    "revision": "9d36c7a5f5dffb0a8722",
+    "url": "/quiz-app/static/css/main.f1558658.chunk.css"
+  },
+  {
+    "revision": "deacecf959496c1df5e7",
+    "url": "/quiz-app/static/js/2.45a5db28.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quiz-app/static/js/2.45a5db28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d36c7a5f5dffb0a8722",
+    "url": "/quiz-app/static/js/main.d53f047b.chunk.js"
+  },
+  {
+    "revision": "d34927bedc2d0ff74f02",
+    "url": "/quiz-app/static/js/runtime-main.807cf86d.js"
+  }
+]);
